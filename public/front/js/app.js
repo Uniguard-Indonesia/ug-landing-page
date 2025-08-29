05d483a176b83212ad00f7a5936cd14be1d93438
Rahma <rahmawnj3@gmail.com>
2025-08-29 14:38:44 +0700

    update

--- public/front/js/app.js
+++ public/front/js/app.js
@@ -1,30 +1,6 @@
 let baseUrl = 'https://uniguard.co.id/api/';
 
-$.ajax({
-  url: baseUrl + 'contact',
-  type: 'GET',
-  success: function (response) {
-    $("#addres").append(response.contact.addres)
-    $("#email").append(response.contact.email)
-    $("#telp").append(response.contact.telp)
-    $("#maps").attr('src', response.contact.maps)
 
-    $("#footer-contact").append(`` + response.contact.addres + ` <br>
-        <strong>Phone:</strong> `+ response.contact.telp + `<br>
-        <strong>Email:</strong> `+ response.contact.email + `<br>`)
-  }
-})
-
-$.ajax({
-  url: baseUrl + 'service',
-  type: 'GET',
-  success: function (response) {
-    $.each(response.service, function (i, data) {
-      $("#our-service").append(`<li><i class="bx bx-chevron-right"></i> <a href="javascript:void()">` + data.title + `</a></li>`)
-    })
-
-  }
-})
 
 $('.btn-send').click(function (e) {
 
